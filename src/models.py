7b5de78c0a8e085ac286d8a1f29d4d6eba1c20ec
IvanGonzalezRecruiter <ivan.gonzalez25@gmail.com>
2022-06-29 15:55:35 +0000

    .

--- src/models.py
+++ src/models.py
@@ -9,55 +9,52 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    __tablename__="user"
+
     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
+    user_name = Column(String(250))
+    first_name = Column(String(10))
+    last_name = Column(String(10))
     email = Column(String(100))
-    password = Column(String(200))
+    password = Column(String(150))
 
-
-class Favorties(Base):
-    __tablename__ = 'favorites'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+    user_to_id = Column(Integer, ForeignKey("user.id"))
+
+class Post(Base):
+    __tablename__= "post"
+
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"))
-    chara_id = Column(Integer, ForeignKey("character.id"))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
 
-class Characters(Base):
-    __tablename__ = "character"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    gender = Column(String(1))
-    height = Column(Integer)
-    mass = Column(Integer)
-    hair_color = Column(String(10))
-    skin_color = Column(String(10))
-    eye_color = Column(String(5))
-    birth_year = Column(String(10))
-    homeworld = Column(String(10))
-    url_image = Column(String(2000))
+class Comment(Base):
+    __tablename__= "comment"
 
-class Planets(Base):
-    __tablename__ = "planet"
     id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    population = Column(Integer)
-    terrain = Column(String(30))
-    diameter = Column(Integer)
-    rotation_period = Column(Integer)
-    orbital_period = Column(Integer)
-    gravity = Column(String(20))
-    climate = Column(String(30))
-    surface_water = Column(Integer)
-    url_image = Column(String(2000))
+    comment_text = Column(String(2200))
+    author_id = Column(Integer, ForeignKey("user.id"))
+    post_id = Column(Integer, ForeignKey("post.id"))
+
+class Media(Base):
+    __tablename__="media"
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50))
+    url = Column(String(2000))
+    post_id = Column(Integer, ForeignKey("post.id"))        
 
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
